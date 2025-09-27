69dd7d38a24e36d58eeee4c5b582f155914f6a7b
matvey <matveykosha7@gmail.com>
2025-09-27 11:00:48 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+const burger = document.getElementById("burger");
+const mobileMenu = document.getElementById("mobileMenu");
+
+burger.addEventListener("click", () => {
+  mobileMenu.style.display =
+    mobileMenu.style.display === "flex" ? "none" : "flex";
+});
